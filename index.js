0037041aa7ccb1bc54fd61eec87814cef14d30c6
Matthew Quigley <quigs.gp@gmail.com>
2021-06-27 15:16:36 -0400

    added functions
    f

--- index.js
+++ index.js
@@ -3,3 +3,26 @@
 a = "Welcome to OpenATS";
 console.log(a);
 
+
+// Reading the PDF input
+
+function readPDF(file) {
+
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+      pdf.src = event.target.result;
+    });
+    reader.readAsArrayBuffer(file);
+  }
+
+
+  // Writing the information to different arrays
+
+function writePDF(file) {
+
+    var name = [];
+    var contacts = [];
+    var experience = [];
+    var skills = [];
+
+}
